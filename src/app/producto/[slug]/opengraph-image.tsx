35e5293902791dc60acee877f551d11786774b89
Felipe Saracho <FelipeSaracho02@gmail.com>
2026-09-12 03:51:24 -0300

    Tamaños explicitos para la imagen

--- src/app/producto/[slug]/opengraph-image.tsx
+++ src/app/producto/[slug]/opengraph-image.tsx
@@ -124,7 +124,7 @@ function Card({ photo }: { photo: string | null }) {
         <img
           alt=""
           src={photo}
-          style={{ height: 620, objectFit: "contain", width: "auto" }}
+          style={{ height: 620, objectFit: "contain", width: 620 }}
         />
       )}
     </div>
